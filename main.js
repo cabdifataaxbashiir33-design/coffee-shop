779bb7bbeab56b28dad3fef5408d179fe31fd902
cabdifataaxbashiir <cabdifataaxbshiir33@gmail.com>
2026-03-19 09:22:54 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// searching find indexoff incude
+
+const fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+// find the first fruit with length less than 6
+const newfruits = fruits.find((fruit) => fruit.length < 6);
+console.log(newfruits);
